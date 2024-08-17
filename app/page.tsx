e40be8ff89bdd980df9765569d8a0deef1bbc376
elcascavel <simao.ramaral@gmail.com>
2024-08-17 19:55:26 +0100

    Use json files

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { BlogPosts } from "app/components/posts";
+import homeData from "./data/home.json";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-medium tracking-tighter">
-        hey, I'm Simão 👋
+        {homeData.intro.title}
       </h1>
       <p className="prose prose-neutral dark:prose-invert">
-        {`I'm a web developer. My last working job was at Imaginary Cloud where I contributed to various internal projects with a focus on frontend development. Previously, I interned as a UX/UI designer at EDP, creating wireframes and final product designs, and as a backend developer at Xplora, developing API endpoints. I hold a BSc in Communication & Multimedia from Universidade de Trás-os-Montes e Alto Douro. I'm currently looking for new opportunities.`}
+        {homeData.intro.description}
       </p>
       <div className="my-8">
         <BlogPosts />
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,50 +1,34 @@
 import type { Metadata } from "next";
+import workData from "../data/work.json";
 
 export const metadata: Metadata = {
-  title: "Work",
-  description: "A summary of my work experience.",
+  title: workData.metadata.title,
+  description: workData.metadata.description,
 };
 
 export default function WorkPage() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">work</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        {workData.metadata.title.toLowerCase()}
+      </h1>
       <div className="prose prose-neutral dark:prose-invert">
-        <p>
-          Below is a summary of my work experience. I'm currently looking for
-          new opportunities.
-        </p>
+        <p>{workData.metadata.description}</p>
         <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        <h2 className="font-medium text-xl mb-1 tracking-tighter">
-          Imaginary Cloud
-        </h2>
-        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-          Web Developer, September 2023 - March 2024
-        </p>
-        <p>
-          My first step in the tech industry was with{" "}
-          <a href="https://www.imaginarycloud.com/">Imaginary Cloud</a> where I
-          contributed to various internal projects with a focus on frontend
-          development. I worked with Next.js, Bootstrap and vanilla HTML/CSS.
-        </p>
-        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        <h2 className="font-medium text-xl mb-1 tracking-tighter">EDP</h2>
-        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-          UX/UI Designer, March 2023 - July 2023 (Internship)
-        </p>
-        <p>
-          I interned at <a href="https://www.edp.com/">EDP</a> where I created
-          wireframes and final product designs. I worked with Figma.
-        </p>
-        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        <h2 className="font-medium text-xl mb-1 tracking-tighter">Xplora</h2>
-        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-          Backend Developer, July 2022 - September 2022 (Internship)
-        </p>
-        <p>
-          I interned at <a href="https://goxplora.com/">Xplora</a> where I
-          developed API endpoints. I worked with Laravel.
-        </p>
+        {workData.workExperience.map((job, index) => (
+          <div key={index}>
+            <h2 className="font-medium text-xl mb-1 tracking-tighter">
+              <a href={job.link}>{job.company}</a>
+            </h2>
+            <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+              {job.role}, {job.duration}
+            </p>
+            <p>{job.description}</p>
+            {index < workData.workExperience.length - 1 && (
+              <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+            )}
+          </div>
+        ))}
       </div>
     </section>
   );
