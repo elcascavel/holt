30a7113ef46f52e61003fda35b6e9b2d68fd2671
elcascavel <simao.amaral@proton.me>
2024-08-22 22:39:18 +0100

    Refactor badge component and update projects data

--- app/components/badge.tsx
+++ app/components/badge.tsx
@@ -1,14 +1,26 @@
+import { SiLaravel } from "react-icons/si";
+import { FaVuejs } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+
 interface BadgeProps {
     text: string;
-    color?: string;
 }
 
-export function Badge({ text, color }: BadgeProps) {
+const badgeStyles: { [key: string]: { color: string; icon: JSX.Element } } = {
+    "Laravel": { color: "bg-red-500 text-white", icon: <SiLaravel className="w-4 h-4" /> },
+    "Vue": { color: "bg-green-500 text-white", icon: <FaVuejs className="w-4 h-4"/> },
+    "Next.js": { color: "bg-black text-white", icon: <RiNextjsFill className="w-4 h-4"/> }
+};
+
+export function Badge({ text }: BadgeProps) {
+    const style = badgeStyles[text] || { color: "bg-neutral-500 text-white", icon: null }; // Fallback style
+
     return (
-        <div className="relative select-none items-center whitespace-nowrap rounded-lg bg-pink-900 py-1.5 px-3 font-sans text-xs font-bold uppercase">
-            <span className="text-pink-300">{text}</span>
+        <div className={`relative select-none flex items-center gap-2 whitespace-nowrap rounded-lg py-1.5 px-3 font-sans text-xs font-bold uppercase ${style.color}`}>
+            {style.icon && style.icon}
+            <span>{text}</span>
         </div>
     );
-};
+}
 
 export default Badge;
